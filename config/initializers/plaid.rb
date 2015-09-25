438029c897616ef1fb4f02ebc30e7256f8be8d08
falese <falese@gmail.com>
2015-09-24 22:25:10 -0700

    commit of plaid.rb and updated sessions controller.rb

---
+++ config/initializers/plaid.rb
@@ -0,0 +1,7 @@
+Plaid.config do |p|
+  p.customer_id = '55d6613794aec9e057dbea50'
+  p.secret = 'd170dcb9b01005357ae6a5b1f0df81'
+  p.environment_location = 'https://tartan.plaid.com/'
+  # i.e. 'https://tartan.plaid.com/' for development, or
+  # 'https://api.plaid.com/' for production
+end
